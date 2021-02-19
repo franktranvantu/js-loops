2b0a6133fe07e9caff21162a71050c698ae6660c
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-02-19 18:01:56 +0700

    Update

--- final/js/script.js
+++ final/js/script.js
@@ -1,17 +1,11 @@
 let html = '';
-let red;
-let green;
-let blue;
-let randomRGB;
 
 const randomValue = () => Math.floor(Math.random() * 256);
 
+const randomRGB = (value) => `rgb( ${value()}, ${value()}, ${value()} )`;
+
 for (let i = 1; i <= 10; i++) {
-  red = randomValue();
-  green = randomValue();
-  blue = randomValue();
-  randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
-  html += `<div style="background-color: ${randomRGB}">${i}</div>`;
+  html += `<div style="background-color: ${randomRGB(randomValue)}">${i}</div>`;
 }
 
 document.querySelector('main').innerHTML = html;
